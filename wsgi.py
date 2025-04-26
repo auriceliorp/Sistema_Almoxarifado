65e4e00284081acbbb266fd26e81a50544078b77
auriceliorp <auriceliorp@gmail.com>
2025-04-26 19:48:55 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,8 @@ import sys
 # Adiciona o diretório atual ao path do Python
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-# Importa o módulo app.py diretamente
-from almoxarifado_app.app import create_app
+# Tenta importar diretamente do arquivo app.py
+from app import create_app
 
 # Cria a aplicação
 app = create_app()

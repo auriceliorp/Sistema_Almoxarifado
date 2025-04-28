4cd8edd1c89ddd21f748b8e91207964996b3ed9f
auriceliorp <auriceliorp@gmail.com>
2025-04-28 20:56:39 -0300

    Update routes_usuario.py

--- routes_usuario.py
+++ routes_usuario.py
@@ -1,10 +1,9 @@
 # routes_usuario.py
-
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required
-from werkzeug.security import generate_password_hash
-from database import db
 from models import Usuario, Perfil
+from database import db
+from werkzeug.security import generate_password_hash
 
 usuario_bp = Blueprint('usuario', __name__, url_prefix='/usuario')
 
@@ -17,38 +16,26 @@ def lista_usuario():
 @usuario_bp.route('/novo', methods=['GET', 'POST'])
 @login_required
 def novo_usuario():
-    perfis = Perfil.query.all()
     if request.method == 'POST':
         nome = request.form.get('nome')
         email = request.form.get('email')
-        senha = request.form.get('senha')
         matricula = request.form.get('matricula')
+        senha = request.form.get('senha')
         perfil_id = request.form.get('perfil_id')
 
-        if Usuario.query.filter_by(email=email).first():
-            flash('E-mail já cadastrado.')
-            return redirect(url_for('usuario.novo_usuario'))
+        senha_hash = generate_password_hash(senha)
 
         novo_usuario = Usuario(
             nome=nome,
             email=email,
-            senha=generate_password_hash(senha),
             matricula=matricula,
+            senha=senha_hash,
             perfil_id=perfil_id
         )
         db.session.add(novo_usuario)
         db.session.commit()
-
-        flash('Usuário cadastrado com sucesso!')
+        flash('Usuário cadastrado com sucesso!', 'success')
         return redirect(url_for('usuario.lista_usuario'))
 
+    perfis = Perfil.query.all()
     return render_template('novo_usuario.html', perfis=perfis)
-
-@usuario_bp.route('/excluir/<int:id>', methods=['POST'])
-@login_required
-def excluir_usuario(id):
-    usuario = Usuario.query.get_or_404(id)
-    db.session.delete(usuario)
-    db.session.commit()
-    flash('Usuário excluído com sucesso!')
-    return redirect(url_for('usuario.lista_usuario'))

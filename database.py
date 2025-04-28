d3ae1ca59db3ad378f5da8d2abbc7dcf1e15337e
auriceliorp <auriceliorp@gmail.com>
2025-04-28 06:50:28 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
-# Instância do SQLAlchemy
 db = SQLAlchemy()
-
-# Instância do Flask-Migrate
-migrate = Migrate()
-
-def init_app(app):
-    """Inicializa as extensões do banco de dados com a aplicação Flask."""
-    db.init_app(app)
-    migrate.init_app(app, db)
-

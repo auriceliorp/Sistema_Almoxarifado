a48b2f43fd67c2d54f805ecf33676716a1aacb14
auriceliorp <auriceliorp@gmail.com>
2025-04-28 13:29:05 -0300

    Update routes_nd.py

--- routes_nd.py
+++ routes_nd.py
@@ -1,4 +1,4 @@
-# routes_nd.py
+# routes_nd.py corrigido
 
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_required
@@ -13,22 +13,25 @@ def lista_nd():
     nds = NaturezaDespesa.query.all()
     return render_template('lista_nd.html', nds=nds)
 
-@nd.route('/cadastrar', methods=['GET', 'POST'])
+@nd.route('/nova', methods=['GET', 'POST'])
 @login_required
 def cadastrar_nd():
     if request.method == 'POST':
         codigo = request.form.get('codigo')
         descricao = request.form.get('descricao')
-
-        if not codigo or not descricao:
-            flash('Código e descrição são obrigatórios.', 'danger')
-            return redirect(url_for('nd.cadastrar_nd'))
-
         nova_nd = NaturezaDespesa(codigo=codigo, descricao=descricao)
         db.session.add(nova_nd)
         db.session.commit()
-
         flash('Natureza de Despesa cadastrada com sucesso!', 'success')
         return redirect(url_for('nd.lista_nd'))
 
     return render_template('cadastrar_nd.html')
+
+@nd.route('/excluir/<int:id>', methods=['POST'])
+@login_required
+def excluir_nd(id):
+    nd = NaturezaDespesa.query.get_or_404(id)
+    db.session.delete(nd)
+    db.session.commit()
+    flash('Natureza de Despesa excluída com sucesso!', 'success')
+    return redirect(url_for('nd.lista_nd'))

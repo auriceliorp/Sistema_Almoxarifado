da123ee1b560613bd698b0bff086da36d5234e9c
auriceliorp <auriceliorp@gmail.com>
2025-04-28 10:08:39 -0300

    Update models.py

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    senha = db.Column(db.String(255), nullable=False)
+    senha = db.Column(db.Text, nullable=False)
     perfil_id = db.Column(db.Integer, db.ForeignKey('perfil.id'), nullable=False)
 
 # Classe Perfil

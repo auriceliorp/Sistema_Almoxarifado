bc5cc0c60a4b2a069bb5db9d744f44de2293c5db
auriceliorp <auriceliorp@gmail.com>
2025-04-28 21:53:05 -0300

    Create routes_estoque.py

---
+++ routes_estoque.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required
+from models import Estoque, Item, NaturezaDespesa
+from database import db
+
+estoque_bp = Blueprint('estoque', __name__, url_prefix='/estoque')
+
+@estoque_bp.route('/')
+@login_required
+def lista_estoque():
+    estoques = Estoque.query.all()
+    return render_template('lista_estoque.html', estoques=estoques)
+
+@estoque_bp.route('/novo', methods=['GET', 'POST'])
+@login_required
+def novo_estoque():
+    if request.method == 'POST':
+        item_id = request.form['item_id']
+        fornecedor = request.form['fornecedor']
+        nota_fiscal = request.form['nota_fiscal']
+        valor_unitario = float(request.form['valor_unitario'])
+        quantidade = int(request.form['quantidade'])
+        local = request.form['local']
+        valor_total = valor_unitario * quantidade
+
+        novo_estoque = Estoque(
+            item_id=item_id,
+            fornecedor=fornecedor,
+            nota_fiscal=nota_fiscal,
+            valor_unitario=valor_unitario,
+            quantidade=quantidade,
+            local=local,
+            valor_total=valor_total
+        )
+
+        db.session.add(novo_estoque)
+
+        # Atualiza o saldo da Natureza de Despesa
+        item = Item.query.get(item_id)
+        if item and item.natureza_despesa:
+            item.natureza_despesa.saldo += valor_total
+
+        db.session.commit()
+
+        flash('Entrada de estoque registrada com sucesso.')
+        return redirect(url_for('estoque.lista_estoque'))
+
+    itens = Item.query.all()
+    return render_template('novo_estoque.html', itens=itens)

02e25c81539b4628f65b181184267857b4294935
auriceliorp <auriceliorp@gmail.com>
2025-04-28 22:37:05 -0300

    Update routes_estoque.py

--- routes_estoque.py
+++ routes_estoque.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from database import db
-from models import Estoque, Item, Fornecedor
+from models import Estoque, Item
 
 estoque_bp = Blueprint('estoque', __name__, url_prefix='/estoque')
 
@@ -17,21 +17,19 @@ def lista_estoque():
 @login_required
 def novo_estoque():
     itens = Item.query.all()
-    fornecedores = Fornecedor.query.all()
-
     if request.method == 'POST':
-        item_id = request.form.get('item_id')
-        fornecedor_id = request.form.get('fornecedor_id')
-        nota_fiscal = request.form.get('nota_fiscal')
-        valor_unitario = float(request.form.get('valor_unitario'))
-        quantidade = int(request.form.get('quantidade'))
-        local = request.form.get('local')
+        item_id = request.form['item_id']
+        fornecedor = request.form['fornecedor']
+        nota_fiscal = request.form['nota_fiscal']
+        valor_unitario = float(request.form['valor_unitario'])
+        quantidade = int(request.form['quantidade'])
+        local = request.form['local']
 
         valor_total = valor_unitario * quantidade
 
         novo_estoque = Estoque(
             item_id=item_id,
-            fornecedor_id=fornecedor_id,
+            fornecedor=fornecedor,
             nota_fiscal=nota_fiscal,
             valor_unitario=valor_unitario,
             quantidade=quantidade,
@@ -41,7 +39,9 @@ def novo_estoque():
 
         db.session.add(novo_estoque)
         db.session.commit()
-        flash('Estoque cadastrado com sucesso!')
+
+        flash('Novo material cadastrado no estoque com sucesso!', 'success')
         return redirect(url_for('estoque.lista_estoque'))
 
-    return render_template('novo_estoque.html', itens=itens, fornecedores=fornecedores)
+    return render_template('novo_estoque.html', itens=itens)
+

f48a9f4a2b6a15dbbd3ddee3fdc45d127f1f430a
auriceliorp <auriceliorp@gmail.com>
2025-05-02 10:53:23 -0300

    Create routes.main.py

---
+++ routes.main.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, logout_user, current_user
+
+main = Blueprint('main', __name__)
+
+@main.route('/home')
+@login_required
+def home():
+    return render_template('home.html', usuario=current_user)
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', usuario=current_user)
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))

fd2202d611313294aa810bc6b7f0c8761ade95f9
auriceliorp <auriceliorp@gmail.com>
2025-05-05 10:33:49 -0300

    Update routes_usuario.py

--- routes_usuario.py
+++ routes_usuario.py
@@ -5,15 +5,19 @@ from flask_login import login_required
 from database import db
 from models import Usuario, Perfil
 from werkzeug.security import generate_password_hash
+from sqlalchemy.orm import joinedload
 
 usuario_bp = Blueprint('usuario_bp', __name__, url_prefix='/usuario')
 
+# ------------------- LISTAGEM DE USUÁRIOS -------------------
 @usuario_bp.route('/')
 @login_required
 def lista_usuario():
-    usuarios = Usuario.query.all()
+    # joinedload força o carregamento da relação 'perfil'
+    usuarios = Usuario.query.options(joinedload(Usuario.perfil)).all()
     return render_template('lista_usuario.html', usuarios=usuarios)
 
+# ------------------- CADASTRO DE NOVO USUÁRIO -------------------
 @usuario_bp.route('/novo', methods=['GET', 'POST'])
 @login_required
 def novo_usuario():
@@ -27,7 +31,7 @@ def novo_usuario():
         perfil_id = request.form.get('perfil_id')
 
         if not (nome and email and senha and perfil_id):
-            flash('Todos os campos são obrigatórios!')
+            flash('Todos os campos obrigatórios devem ser preenchidos!', 'warning')
             return redirect(url_for('usuario_bp.novo_usuario'))
 
         senha_hash = generate_password_hash(senha)
@@ -39,16 +43,21 @@ def novo_usuario():
             matricula=matricula,
             perfil_id=perfil_id
         )
+
         db.session.add(novo_usuario)
         db.session.commit()
 
-        flash('Usuário criado com sucesso!')
+        flash('Usuário criado com sucesso!', 'success')
         return redirect(url_for('usuario_bp.lista_usuario'))
 
     return render_template('novo_usuario.html', perfis=perfis)
 
+# ------------------- EXCLUSÃO DE USUÁRIO -------------------
 @usuario_bp.route('/excluir/<int:id>', methods=['POST'])
 @login_required
 def excluir_usuario(id):
     usuario = Usuario.query.get_or_404(id)
-    db.session.delete
+    db.session.delete(usuario)
+    db.session.commit()
+    flash('Usuário excluído com sucesso!', 'success')
+    return redirect(url_for('usuario_bp.lista_usuario'))

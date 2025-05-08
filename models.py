36641b6dff0d1da0ab1ec03ba292c0d5b15248d2
auriceliorp <auriceliorp@gmail.com>
2025-05-08 11:28:36 -0300

    Atualizar o models.py

--- models.py
+++ models.py
@@ -127,7 +127,7 @@ class EntradaMaterial(db.Model):
     data_nota_fiscal = db.Column(db.Date, nullable=False)
     numero_nota_fiscal = db.Column(db.String(50), nullable=False)
 
- fornecedor_id = db.Column(db.Integer, db.ForeignKey('fornecedores.id'), nullable=False)
+    fornecedor_id = db.Column(db.Integer, db.ForeignKey('fornecedores.id'), nullable=False)
 
     fornecedor = db.relationship('Fornecedor', backref='entradas')
     itens = db.relationship('EntradaItem', backref='entrada_material', cascade="all, delete-orphan")

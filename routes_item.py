ffe1a8671b739f38f77a43d93c3f6a504ca374ef
auriceliorp <auriceliorp@gmail.com>
2025-05-09 10:35:02 -0300

    Update routes_item.py

--- routes_item.py
+++ routes_item.py
@@ -37,7 +37,7 @@ def detalhes_item(id):
     # Busca o item pelo ID
     item = Item.query.get_or_404(id)
 
-    return render_template('detalhes_item.html', item=item)
+    return render_template('detalhar_item.html', item=item)
 
 
     return send_file(pdf_bytes, download_name="itens.pdf", as_attachment=True)

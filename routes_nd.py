a4b9f31703f883402c59afa7825a0a4bae4f112c
auriceliorp <auriceliorp@gmail.com>
2025-05-13 08:36:56 -0300

    Update routes_nd.py

--- routes_nd.py
+++ routes_nd.py
@@ -1,18 +1,22 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from extensoes import db
 from models import NaturezaDespesa
 
 nd_bp = Blueprint('nd_bp', __name__, url_prefix='/nd')
 
-# -------------------------- LISTA ND (retorno HTML parcial) -------------------------- #
+def is_ajax_request():
+    return request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+# ------------------------------ LISTAGEM ------------------------------ #
 @nd_bp.route('/')
 @login_required
 def lista_nd():
     nds = NaturezaDespesa.query.order_by(NaturezaDespesa.codigo).all()
-    return render_template('lista_nd.html', nds=nds)
+    template = 'partials/nd/lista_nd.html' if is_ajax_request() else 'nd_grupos_ul.html'
+    return render_template(template, nds=nds)
 
-# -------------------------- NOVA ND -------------------------- #
+# ------------------------------ NOVA ND ------------------------------ #
 @nd_bp.route('/novo', methods=['GET', 'POST'])
 @login_required
 def nova_nd():
@@ -21,38 +25,45 @@ def nova_nd():
         nome = request.form.get('nome')
 
         if not codigo or not nome:
-            return jsonify({'erro': 'Preencha todos os campos obrigatórios.'}), 400
+            flash('Preencha todos os campos obrigatórios.')
+            return render_template('partials/nd/form_nd.html', nd=None)
 
         nova = NaturezaDespesa(codigo=codigo, nome=nome)
         db.session.add(nova)
         db.session.commit()
-        return jsonify({'mensagem': 'ND criada com sucesso'}), 201
+        flash('Natureza de Despesa cadastrada com sucesso!')
+        nds = NaturezaDespesa.query.order_by(NaturezaDespesa.codigo).all()
+        return render_template('partials/nd/lista_nd.html', nds=nds)
 
-    return render_template('form_nd.html', nd=None)
+    return render_template('partials/nd/form_nd.html', nd=None)
 
-# -------------------------- EDITAR ND -------------------------- #
+# ------------------------------ EDITAR ND ------------------------------ #
 @nd_bp.route('/editar/<int:id>', methods=['GET', 'POST'])
 @login_required
 def editar_nd(id):
     nd = NaturezaDespesa.query.get_or_404(id)
-
     if request.method == 'POST':
         nd.codigo = request.form.get('codigo')
         nd.nome = request.form.get('nome')
 
         if not nd.codigo or not nd.nome:
-            return jsonify({'erro': 'Preencha todos os campos obrigatórios.'}), 400
+            flash('Preencha todos os campos obrigatórios.')
+            return render_template('partials/nd/form_nd.html', nd=nd)
 
         db.session.commit()
-        return jsonify({'mensagem': 'ND atualizada com sucesso'}), 200
+        flash('Natureza de Despesa atualizada com sucesso!')
+        nds = NaturezaDespesa.query.order_by(NaturezaDespesa.codigo).all()
+        return render_template('partials/nd/lista_nd.html', nds=nds)
 
-    return render_template('form_nd.html', nd=nd)
+    return render_template('partials/nd/form_nd.html', nd=nd)
 
-# -------------------------- EXCLUIR ND -------------------------- #
+# ------------------------------ EXCLUIR ND ------------------------------ #
 @nd_bp.route('/excluir/<int:id>', methods=['POST'])
 @login_required
 def excluir_nd(id):
     nd = NaturezaDespesa.query.get_or_404(id)
     db.session.delete(nd)
     db.session.commit()
-    return jsonify({'mensagem': 'ND excluída com sucesso'}), 200
+    flash('Natureza de Despesa excluída com sucesso!')
+    nds = NaturezaDespesa.query.order_by(NaturezaDespesa.codigo).all()
+    return render_template('partials/nd/lista_nd.html', nds=nds)

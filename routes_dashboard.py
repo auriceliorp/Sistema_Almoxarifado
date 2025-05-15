11a900406daedaa0547d33e3047e5700d0ee4941
auriceliorp <auriceliorp@gmail.com>
2025-05-15 04:04:03 -0300

    Atualizar o routes_dashboard.py

--- routes_dashboard.py
+++ routes_dashboard.py
@@ -5,7 +5,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from sqlalchemy import extract, func
 from datetime import datetime
-from modelos import EntradaItem, SaidaItem, EntradaMaterial, SaidaMaterial, NaturezaDespesa
+from models import EntradaItem, SaidaItem, EntradaMaterial, SaidaMaterial, NaturezaDespesa
 
 dashboard_bp = Blueprint('dashboard_bp', __name__, url_prefix='/dashboard')
 

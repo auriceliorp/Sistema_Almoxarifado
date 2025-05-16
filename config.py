c2aed930579bb4418cd822b4153ba82ec99609c7
auriceliorp <auriceliorp@gmail.com>
2025-05-16 15:36:23 -0300

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     # Configuração da URI do banco de dados (usada pelo SQLAlchemy)
     # Em produção (Render), ela é definida diretamente como variável de ambiente
     # Exemplo: postgresql://usuario:senha@host:porta/nomedobanco
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or os.environ.get('DATABASE_URL')
 
     # Desativa o rastreamento de modificações do SQLAlchemy para melhorar desempenho
     SQLALCHEMY_TRACK_MODIFICATIONS = False

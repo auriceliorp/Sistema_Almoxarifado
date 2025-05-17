1891854dfafab0b8d59946e64caa3f608b5e0ed1
auriceliorp <auriceliorp@gmail.com>
2025-05-17 14:52:06 -0300

    Atualizar o app_render.py

--- app_render.py
+++ app_render.py
@@ -83,6 +83,7 @@ def create_app():
     from routes_saida import saida_bp
     from routes_relatorio import relatorio_bp
     from routes_dashboard import dashboard_bp
+    from routes_popular import popular_bp  # <-- NOVO
 
     app.register_blueprint(main)
     app.register_blueprint(usuario_bp)
@@ -95,6 +96,7 @@ def create_app():
     app.register_blueprint(saida_bp)
     app.register_blueprint(relatorio_bp)
     app.register_blueprint(dashboard_bp)
+    app.register_blueprint(popular_bp)  # <-- NOVO
 
     return app
 

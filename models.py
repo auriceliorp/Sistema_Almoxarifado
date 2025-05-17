d5a3e49f46c4dda0ddb288078c7fd7fc78371363
auriceliorp <auriceliorp@gmail.com>
2025-05-16 21:26:57 -0300

    Update models.py

--- models.py
+++ models.py
@@ -83,9 +83,19 @@ class Fornecedor(db.Model):
     cnpj = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), nullable=True)
     telefone = db.Column(db.String(20), nullable=True)
+    celular = db.Column(db.String(20), nullable=True)
+    endereco = db.Column(db.String(200), nullable=True)
+    numero = db.Column(db.String(10), nullable=True)
+    complemento = db.Column(db.String(100), nullable=True)
+    cep = db.Column(db.String(10), nullable=True)
+    cidade = db.Column(db.String(100), nullable=True)
+    uf = db.Column(db.String(2), nullable=True)
+    inscricao_estadual = db.Column(db.String(50), nullable=True)
+    inscricao_municipal = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
         return f'<Fornecedor {self.nome}>'
+
 
 # ------------------- LOCAL E UNIDADE LOCAL -------------------
 class Local(db.Model):

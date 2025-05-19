df65a3d9785a46fe048a49e91945329dab85342b
auriceliorp <auriceliorp@gmail.com>
2025-05-19 12:56:16 -0300

    Update models.py

--- models.py
+++ models.py
@@ -251,6 +251,8 @@ class PainelContratacao(db.Model):
     responsavel_conducao = db.Column(db.String(100), nullable=True)
     setor_responsavel = db.Column(db.String(100), nullable=True)
     status = db.Column(db.String(50), nullable=True)
+    excluido = db.Column(db.Boolean, default=False)
+
 
     def __repr__(self):
         return f"<PainelContratacao {self.numero_sei}>"

5896541eb6daf4cc07f6a2c3d360865530dd30ac
auriceliorp <auriceliorp@gmail.com>
2025-05-21 06:43:14 -0300

    Atualizar o routes_nd.py

--- routes_nd.py
+++ routes_nd.py
@@ -1,69 +1,56 @@
 # routes_nd.py
-# Rotas para Natureza de Despesa (ND)
 
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
-from extensoes import db
+from database import db
 from models import NaturezaDespesa
 
+# Criação do blueprint da natureza de despesa
 nd_bp = Blueprint('nd_bp', __name__, url_prefix='/nd')
 
-# ------------------------------ LISTAGEM ------------------------------ #
+# ------------------------- ROTA: LISTAR ND ------------------------- #
 @nd_bp.route('/')
 @login_required
 def lista_nd():
-    nds = NaturezaDespesa.query.order_by(NaturezaDespesa.codigo).all()
-    return render_template('partials/nd/lista_nd.html', nds=nds)
+    """
+    Lista todas as Naturezas de Despesa cadastradas.
+    """
+    nds = NaturezaDespesa.query.all()
+    return render_template('partials/nd/lista.html', nds=nds)
 
-# ------------------------------ NOVA ND ------------------------------ #
+# ------------------------- ROTA: NOVA ND ------------------------- #
 @nd_bp.route('/novo', methods=['GET', 'POST'])
 @login_required
-def nova_nd():
+def novo_nd():
+    """
+    Cadastra uma nova Natureza de Despesa.
+    """
     if request.method == 'POST':
         codigo = request.form.get('codigo')
         nome = request.form.get('nome')
-
         if not codigo or not nome:
             flash('Preencha todos os campos obrigatórios.')
-            return render_template('partials/nd/form_nd.html', nd=None)
-
-        nova = NaturezaDespesa(codigo=codigo, nome=nome)
-        db.session.add(nova)
+            return redirect(url_for('nd_bp.novo_nd'))
+        nova_nd = NaturezaDespesa(codigo=codigo, nome=nome)
+        db.session.add(nova_nd)
         db.session.commit()
         flash('Natureza de Despesa cadastrada com sucesso!')
         return redirect(url_for('nd_bp.lista_nd'))
+    return render_template('partials/nd/form.html')
 
-    return render_template('partials/nd/form_nd.html', nd=None)
-
-# ------------------------------ EDITAR ND ------------------------------ #
+# ------------------------- ROTA: EDITAR ND ------------------------- #
 @nd_bp.route('/editar/<int:id>', methods=['GET', 'POST'])
 @login_required
 def editar_nd(id):
+    """
+    Edita uma Natureza de Despesa existente.
+    """
     nd = NaturezaDespesa.query.get_or_404(id)
-
     if request.method == 'POST':
-        codigo = request.form.get('codigo')
-        nome = request.form.get('nome')
-
-        if not codigo or not nome:
-            flash('Preencha todos os campos obrigatórios.')
-            return render_template('partials/nd/form_nd.html', nd=nd)
-
-        nd.codigo = codigo
-        nd.nome = nome
+        nd.codigo = request.form.get('codigo')
+        nd.nome = request.form.get('nome')
         db.session.commit()
         flash('Natureza de Despesa atualizada com sucesso!')
         return redirect(url_for('nd_bp.lista_nd'))
-
-    return render_template('partials/nd/form_nd.html', nd=nd)
-
-# ------------------------------ EXCLUIR ND ------------------------------ #
-@nd_bp.route('/excluir/<int:id>', methods=['POST'])
-@login_required
-def excluir_nd(id):
-    nd = NaturezaDespesa.query.get_or_404(id)
-    db.session.delete(nd)
-    db.session.commit()
-    flash('Natureza de Despesa excluída com sucesso!')
-    return redirect(url_for('nd_bp.lista_nd'))
+    return render_template('partials/nd/form.html', nd=nd)
 

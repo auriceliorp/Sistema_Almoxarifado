9bf15604d543e7ca9ca03e570c26e1af7a55fff6
auriceliorp <auriceliorp@gmail.com>
2025-05-27 12:00:21 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-# ------------------------------ IMPORTAÇÕES ------------------------------
+    # ------------------------------ IMPORTAÇÕES ------------------------------
 from flask_login import UserMixin
 from extensoes import db
 from datetime import datetime
@@ -266,6 +266,10 @@ class BemPatrimonial(db.Model):
     situacao = db.Column(db.String(50), default='Em uso')  # Novo campo
     data_cadastro = db.Column(db.DateTime, default=datetime.utcnow)
     observacoes = db.Column(db.Text, nullable=True)
+    tipo_bem_id = db.Column(db.Integer, db.ForeignKey('tipos_bem.id'), nullable=True)
+    tipo_bem = db.relationship('TipoBem', backref='bens')
+    local_id = db.Column(db.Integer, db.ForeignKey('local.id'), nullable=True)
+    local = db.relationship('Local', backref='bens')
 
     def __repr__(self):
         return f"<BemPatrimonial {self.numero_ul} - {self.nome}>"

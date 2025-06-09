436da13f8cae9a2b17dddf4cefbdb0c7abf00b5c
auriceliorp <auriceliorp@gmail.com>
2025-06-09 15:15:08 -0300

    Update extensoes.py

--- extensoes.py
+++ extensoes.py
@@ -1,12 +1,10 @@
 # extensoes.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
 # Cria instâncias das extensões
 db = SQLAlchemy()
 login_manager = LoginManager()
-migrate = Migrate()
 csrf = CSRFProtect()
 

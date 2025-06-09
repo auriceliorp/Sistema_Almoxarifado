1c603d17cb0d17d81d1e86467df0985a808c9dae
auriceliorp <auriceliorp@gmail.com>
2025-06-09 13:17:07 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import sys
 # Adiciona o diretório raiz ao PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-from app_render import app
+from cli import app
 
 if __name__ == "__main__":
     app.run()

20ea2a81b99b22281e5c22fe168443dabad14807
auriceliorp <auriceliorp@gmail.com>
2025-06-09 13:03:11 -0300

    Create cli.py

---
+++ cli.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import click
+from flask.cli import FlaskGroup
+
+# Adiciona o diretório raiz ao PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from app_render import app, db
+from flask_migrate import Migrate
+
+# Inicializa o Flask-Migrate
+migrate = Migrate(app, db)
+
+def create_cli_app():
+    return app
+
+cli = FlaskGroup(create_app=create_cli_app)
+
+if __name__ == '__main__':
+    cli()

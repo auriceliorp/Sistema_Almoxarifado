e6810b2cb785e683ae3cf889c4e300adbb5487ac
auriceliorp <auriceliorp@gmail.com>
2025-06-27 11:04:47 -0300

    Update 011_atualizar_status_solicitacao.sql

--- migrations/011_atualizar_status_solicitacao.sql
+++ migrations/011_atualizar_status_solicitacao.sql
@@ -1,15 +1,14 @@
--- Atualizar status das solicitações existentes
 UPDATE solicitacao_compra
 SET status = 'Pendente'
 WHERE status IN ('PENDENTE');
 
 UPDATE solicitacao_compra
-SET status = 'Processo Iniciado'
-WHERE status IN ('PENDENTE', 'PROCESSO_INICIADO', 'Processo Iniciado');
-
-UPDATE solicitacao_compra
 SET status = 'Em Andamento'
 WHERE status IN ('EM_ANALISE', 'EM_ANDAMENTO', 'Em andamento');
+
+UPDATE solicitacao_compra
+SET status = 'Processo Iniciado'
+WHERE status IN ('PROCESSO_INICIADO', 'Processo Iniciado');
 
 UPDATE solicitacao_compra
 SET status = 'Concluido'
@@ -22,4 +21,4 @@ WHERE status IN ('REJEITADA', 'CANCELADA', 'Cancelada');
 -- Atualizar outros status que possam existir
 UPDATE solicitacao_compra
 SET status = 'Aguardando Definições'
-WHERE status IN ('AGUARDANDO_DEFINICOES', 'Aguardando Definições'); 
+WHERE status IN ('AGUARDANDO_DEFINICOES', 'Aguardando Definições');
